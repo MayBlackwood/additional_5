a222ec41ed895b2fc6c893a5d917936ae7595bed
Ksenia <36792023+MayBlackwood@users.noreply.github.com>
2018-09-30 22:57:15 +0300

    fixed problem

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-module.exports =function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
 
-str = str.trim().split(''); 
-str = str.filter(el => { 
-return el !== ' '; 
-}) 
-let prov = 0; 
-while (str.length !== 0 && prov !== str.length){ 
-prov = str.length; 
-str.forEach((el, index, arr) => { 
-bracketsConfig.forEach(brackets => { 
-if (el === brackets[0].trim() && arr[index + 1] === brackets[1].trim()) { 
-arr[index] = true; 
-arr[index + 1] = true; 
-} 
-}) 
-}) 
-str = str.filter(el => { 
-return el !== true; 
-}) 
-} 
-
-if(str.length === 0) 
-return true; 
-else 
-return false; 
-}
+    str = str.trim().split(''); 
+    str = str.filter(el => { 
+    return el !== ' '; 
+    }) 
+    let prov = 0; 
+    while (str.length !== 0 && prov !== str.length){ 
+    prov = str.length; 
+    str.forEach((el, index, arr) => { 
+    bracketsConfig.forEach(brackets => { 
+    if (el === brackets[0].trim() && arr[index + 1] === brackets[1].trim()) { 
+    arr[index] = true; 
+    arr[index + 1] = true; 
+    } 
+    }) 
+    }) 
+    str = str.filter(el => { 
+    return el !== true; 
+    }) 
+    } 
+    
+    if(str.length === 0) 
+    return true; 
+    else 
+    return false; 
+    }
